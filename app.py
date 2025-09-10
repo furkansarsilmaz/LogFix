86ecab73321ca0f7039d7adb729f92058666967b
furkansarsilmaz <160139624+furkansarsilmaz@users.noreply.github.com>
2025-09-10 21:33:27 +0300

    working on pages

--- app.py
+++ app.py
@@ -7,17 +7,32 @@ def hello_world():
     return render_template("index.html")
 
 
+# functions for analyzing as log level
 @app.route("/log_level_page", methods=["POST"])
 def selam():
+    """
+    this function renders html when clicked on
+    choosing analyze style
+    """
     return render_template("log_level.html")
 
 @app.route("/analyze_as_log",methods=["POST"])
 def analyze():
+    """
+    algorithm for analyzing at log level
+    """
     return "<p> WORKIN </p>"
 
-@app.route("/analyze",methods=["POST"])
-def analyze_2():
-    return "<p> ITS GONNA LOOK FOR DATES ON ANALYZE </p>"
+
+
+#functions for analyzing with date
+@app.route("/date_level_page",methods=["POST"])
+def analyze_date_page():
+    return render_template("date_level.html")
+
+@app.route("/analyze_as_date",methods=["POST"])
+def analyze_date():
+    return "<p> ANALYZING WITH DATE </p>"
 
 if __name__ == "__main__":
     app.run(debug=True)
