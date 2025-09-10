f59540cd79e15ec2fd048de8531ba70e4ddd1418
furkansarsilmaz <160139624+furkansarsilmaz@users.noreply.github.com>
2025-09-10 15:58:44 +0300

    working on main screen

--- app.py
+++ app.py
@@ -6,5 +6,18 @@ app = Flask(__name__)
 def hello_world():
     return render_template("index.html")
 
+
+@app.route("/log_level_page", methods=["POST"])
+def selam():
+    return render_template("log_level.html")
+
+@app.route("/analyze_as_log",methods=["POST"])
+def analyze():
+    return "<p> WORKIN </p>"
+
+@app.route("/analyze",methods=["POST"])
+def analyze_2():
+    return "<p> ITS GONNA LOOK FOR DATES ON ANALYZE </p>"
+
 if __name__ == "__main__":
     app.run(debug=True)
